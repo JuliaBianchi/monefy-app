f83c0f27c69021ff752e7e569b47ec1a8f8a107a
Júlia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-05-08 20:52:46 -0300

    Cria CrudTransactionActivity
    - Cria uma nova atividade `CrudTransactionActivity` que estende `AppCompatActivity`.
    - Define o layout para `activity_crud_transaction.xml`.
    - Inclui configurações iniciais padrão do Android Studio para atividades.

---
+++ app/src/main/java/com/example/app_moneyfy/CrudTransactionActivity.kt
@@ -0,0 +1,16 @@
+package com.example.app_moneyfy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CrudTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_crud_transaction)
+
+    }
+}
