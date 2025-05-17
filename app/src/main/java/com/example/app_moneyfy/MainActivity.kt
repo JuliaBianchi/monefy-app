a4158b74affdf35259cd8d0d143fc7169b8e8077
Júlia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-05-17 14:46:09 -0300

    Integra DatabaseHandler com MainActivity para exibir transações
    - Inicializa DatabaseHandler em `onCreate`.
    - Configura RecyclerView e seu adaptador para exibir transações.
    - Adiciona `loadTransactions()` para buscar transações do banco de dados e atualizar a UI.
    - Exibe uma mensagem de "Nenhum lançamento encontrado" quando a lista de transações está vazia.
    - Atualiza a lista de transações ao retornar para a atividade (em `onResume`).

--- app/src/main/java/com/example/app_moneyfy/MainActivity.kt
+++ app/src/main/java/com/example/app_moneyfy/MainActivity.kt
@@ -2,21 +2,61 @@ package com.example.app_moneyfy
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import database.DatabaseHandler
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db: DatabaseHandler
+    private lateinit var transactionsAdapter: TransactionsAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var emptyListTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        db = DatabaseHandler(this)
+
+        recyclerView = findViewById(R.id.recycler_view_transactions)
+        emptyListTextView = findViewById(R.id.text_view_empty_list)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        transactionsAdapter = TransactionsAdapter(mutableListOf())
+        recyclerView.adapter = transactionsAdapter
 
         val fab = findViewById<FloatingActionButton>(R.id.transaction_button)
 
         fab.setOnClickListener {
             val intent = Intent(this, CrudTransactionActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadTransactions()
+    }
+
+    private fun loadTransactions() {
+        val transactions = db.list()
+
+        if (transactions.isEmpty()) {
+            recyclerView.visibility = View.GONE
+            emptyListTextView.visibility = View.VISIBLE
+            emptyListTextView.text = "Nenhum lançamento encontrado."
+        } else {
+            recyclerView.visibility = View.VISIBLE
+            emptyListTextView.visibility = View.GONE
+            transactionsAdapter.updateData(transactions)
         }
     }
 }
